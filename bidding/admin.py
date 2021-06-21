bc5ed5c872361670103af33f3abd8f57d77f6363
ajaturner <>
2021-06-21 21:20:26 +0100

    Import export working

--- bidding/admin.py
+++ bidding/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export.formats.base_formats import CSV
 
 from .models import Item, Bid
 
-admin.site.register(Item)
+
+class CustomImportExportModelAdmin(ImportExportModelAdmin):
+  formats = [CSV]
+
+
+@admin.register(Item)
+class ItemAdmin(CustomImportExportModelAdmin):
+  pass
+
+
 admin.site.register(Bid)
--- biddingsoftware/settings.py
+++ biddingsoftware/settings.py
@@ -139,3 +139,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+IMPORT_EXPORT_USE_TRANSACTIONS = True
