e8ba0120a5d9025765330b60b22ecaa5c92573c2
ajaturner <>
2021-06-21 17:35:30 +0100

    Modified settings to test again

--- biddingsoftware/settings.py
+++ biddingsoftware/settings.py
@@ -79,14 +79,14 @@ DEBUG = False
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         },
-#     }
-# else:
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
