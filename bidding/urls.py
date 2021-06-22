2276a7fe13760d48b2fb1d0bb2be16ff8d33c2c2
ajaturner <>
2021-06-23 00:38:32 +0100

    Title

--- bidding/urls.py
+++ bidding/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
     path('', views.name_input, name='name_input'),
     path('bidding/', views.bidding, name='bidding'),
     path('bidding/update_bids/', views.update_bids, name='update_bids'),
