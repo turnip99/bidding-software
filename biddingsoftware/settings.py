85599931df96fa98429f25316d781a080770c5e3
adamturner <adam.turner@vectare.co.uk>
2022-06-07 17:32:40 +0100

    Reverse static file fix

--- biddingsoftware/settings.py
+++ biddingsoftware/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
