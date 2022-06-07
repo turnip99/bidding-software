55a34fb41a95993cf8ef31c7bddc2552c106543b
adamturner <adam.turner@vectare.co.uk>
2022-06-07 17:23:34 +0100

    Attempt to fix static files 2

--- biddingsoftware/urls.py
+++ biddingsoftware/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,6 +21,3 @@ urlpatterns = [
     path('', include('bidding.urls')),
     path('admin/', admin.site.urls),
 ]
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
