2aa9e170f0517fbe3185d964aeff3faa32b60535
adamturner <adam.turner@vectare.co.uk>
2022-06-07 17:42:26 +0100

    Try and fix static files (3)

--- biddingsoftware/urls.py
+++ biddingsoftware/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path('', include('bidding.urls')),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + urlpatterns
