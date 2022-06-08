528e93c1f2d749d0264b126881637e965cb4db38
adamturner <adam.turner@vectare.co.uk>
2022-06-08 10:32:19 +0100

    Moved static files directory

--- biddingsoftware/settings.py
+++ biddingsoftware/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
